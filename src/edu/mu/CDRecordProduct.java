87c0c21713e6986bd5c438a8466a7cd522424234
davidAllenStephan <david.marino03@hotmail.com>
2024-02-12 15:31:30 -0600

    created the CDRecordProduct class and sub classes

---
+++ src/edu/mu/CDRecordProduct.java
@@ -0,0 +1,23 @@
+package edu.mu;
+
+public class CDRecordProduct extends MediaProduct {
+	
+	public CDRecordProduct() {
+		
+	}
+	
+	public CDRecordProduct(String title, double price, int year, Genre genre) {
+		super.title = title;
+		super.price = price;
+		super.year = year;
+		super.genre = genre;
+	}
+	
+	public copy(CDRecordProduct cdrp) {
+		this.title = cdrp.title;
+		this.price = cdrp.price;
+		this.year = cdrp.year;
+		this.genre = cdrp.genre;
+	}
+	
+}
---
+++ src/edu/mu/TapeRecordProduct.java
@@ -0,0 +1,23 @@
+package edu.mu;
+
+public class TapeRecordProduct extends MediaProduct {
+	
+	public TapeRecordProduct() {
+		
+	}
+	
+	public TapeRecordProduct(String title, double price, int year, Genre genre) {
+		super.title = title;
+		super.price = price;
+		super.year = year;
+		super.genre = genre;
+	}
+	
+	public copy(TapeRecordProduct trp) {
+		this.title = trp.title;
+		this.price = trp.price;
+		this.year = trp.year;
+		this.genre = trp.genre;
+	}
+	
+}
